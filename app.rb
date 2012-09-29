aa01fe3bcac53cebd3ecc4af7a54f5d20d551ae5
Joseph Silvashy <jpsilvashy@gmail.com>
2012-09-29 15:25:36 -0700

    restructure files

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ require 'data_mapper'
 set :server, 'thin'
 set :session_secret, 'super-secret'
 set :protection, :except => :frame_options
+set :views, Proc.new { File.join(root, "app/views") }
 
 set :streams, []
 
@@ -26,16 +27,16 @@ set :streams, []
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3::memory:")
 
 # Models
-require_relative 'models/channel'
-require_relative 'models/message'
-require_relative 'models/user'
+require_relative 'app/models/channel'
+require_relative 'app/models/message'
+require_relative 'app/models/user'
 
 # Finalize DataMapper after initializing models
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
 # Controllers
-require_relative 'controllers/base_controller'
-require_relative 'controllers/channel_controller'
-require_relative 'controllers/stream_controller'
+require_relative 'app/controllers/base_controller'
+require_relative 'app/controllers/channel_controller'
+require_relative 'app/controllers/stream_controller'
 
